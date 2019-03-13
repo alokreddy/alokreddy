4f1c471662a30e7e2ef82ed625d953ef8903cfd6
Alok Reddy <alokreddyk@gmail.com>
2019-03-13 19:19:31 +0530

    Updates

---
+++ precache-manifest.cafc5770263cb62efac7c722e60e4b36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3737e2279336fa0afb45",
+    "url": "/alokreddy/static/js/runtime~main.532b2d19.js"
+  },
+  {
+    "revision": "7233bb8781c92bd41a1a",
+    "url": "/alokreddy/static/js/main.f2b7c3ed.chunk.js"
+  },
+  {
+    "revision": "16b6b783c35a69c6d61e",
+    "url": "/alokreddy/static/js/2.a02d8c0c.chunk.js"
+  },
+  {
+    "revision": "7233bb8781c92bd41a1a",
+    "url": "/alokreddy/static/css/main.a8f740d0.chunk.css"
+  },
+  {
+    "revision": "3137cc521e003172a6658dfe5dc691ec",
+    "url": "/alokreddy/index.html"
+  }
+];
